db3eac0e98487041e7942d587b36561780a123d3
lggg2112 <lggg2112@hanmail.net>
2019-11-19 00:08:22 -0600

    Add code related to devise to development.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'store/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'store#index'
 end
